509878070860f733c8adf513fd07601e941e02f9
Vitalii <vitaliyplys1@gmail.com>
2023-10-11 11:36:14 +0300

    add hw solution create-online-book

---
+++ src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,52 @@
+package com.example.onlinebookstore.repository.impl;
+
+import com.example.onlinebookstore.exeption.DataProcessingException;
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can`t save book to DB: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Book ", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t find books from DB: ", e);
+        }
+    }
+}
