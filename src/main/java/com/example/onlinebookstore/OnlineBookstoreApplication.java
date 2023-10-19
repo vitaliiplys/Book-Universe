68ec0e79c22d7f439c699dceddc51d7de4882007
Vitalii <vitaliyplys1@gmail.com>
2023-10-19 22:21:21 +0300

    add hw solution createDto bookstore

--- src/main/java/com/example/onlinebookstore/OnlineBookstoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookstoreApplication.java
@@ -1,34 +1,12 @@
 package com.example.onlinebookstore;
 
-import com.example.onlinebookstore.model.Book;
-import com.example.onlinebookstore.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookstoreApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookstoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book secret = new Book();
-            secret.setTitle("Secret");
-            secret.setAuthor("Rhonda Byrne");
-            secret.setIsbn("12345");
-            secret.setPrice(BigDecimal.valueOf(199));
-
-            bookService.save(secret);
-            System.out.println(bookService.findAll());
-        };
     }
 }
--- src/main/java/com/example/onlinebookstore/service/BookService.java
+++ src/main/java/com/example/onlinebookstore/service/BookService.java
@@ -1,10 +1,14 @@
 package com.example.onlinebookstore.service;
 
-import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.dto.BookDto;
+import com.example.onlinebookstore.dto.CreateBookRequestDto;
 import java.util.List;
 
 public interface BookService {
-    Book save(Book book);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto findBookById(Long id);
+
+    BookDto save(CreateBookRequestDto bookDto);
 }
